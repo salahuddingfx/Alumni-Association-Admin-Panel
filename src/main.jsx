a2114e49402911f20ace2121aa84bb23e60e80cf
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-29 16:39:23 +0600

    feat: initialize React entry point with BrowserRouter configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx';
 import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
